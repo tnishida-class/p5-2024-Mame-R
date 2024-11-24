0e117c8c6204d6e3eccce81d5e144ed9cc5c1128
Mame-R <183998270+Mame-R@users.noreply.github.com>
2024-11-24 21:49:50 +0900

    3-3, 4-2 5-1

--- section3-3/sketch.js
+++ section3-3/sketch.js
@@ -4,22 +4,34 @@ let x, y;
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x = width / 2;
-  y = height / 2;
+  y = windowHeight-25
 }
 
-function draw(){
+function draw() {
   background(160, 192, 255);
-  ellipse(x, windowHeight-25, 50);
-  if(keyIsDown(LEFT_ARROW)){ x -= 5; }
-  if(keyIsDown(RIGHT_ARROW)){ x += 5; }
-  //if(keyIsDown(UP_ARROW)){ y -= 5; }
-  //if(keyIsDown(DOWN_ARROW)){ y += 5; }
-  if(keyIsDown("P".charCodeAt(0))){ x+= 10; }
-  if(keyIsDown("Q".charCodeAt(0))){ x-= 10; }
+  ellipse(x, y, 50);
 
- }
 
+let speed = 5; // 通常の移動速度
+
+  // Sキーが押されている場合に加速
+  if (keyIsDown("S".charCodeAt(0))) {
+  speed = 10;
+  }
+  
+  // 左矢印キーで移動
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= speed;
+  }
+
+  // 右矢印キーで移動
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += speed;
+  }
+
+  
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
+}
 }
